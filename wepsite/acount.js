d57b7ff5dfc05c132f1df9946e75d178e95ba011
Sedatrusen <56399517+Sedatrusen@users.noreply.github.com>
2019-10-23 23:08:43 +0300

    cant login

--- wepsite/acount.js
+++ wepsite/acount.js
@@ -17,21 +17,22 @@ var borders=new Vue({
     
     data:{
 
-namecolor: '#10a1de',
-tarihcolor:'#10a1de',
-daycolor:'#10a1de',
-surnamecolor:'#10a1de',
-passcolor:'#10a1de',
-answercolor:'#10a1de',
-yearcolor:'#10a1de',
-emailcolor:'#10a1de',
+namecolor: 'rgba(255,255,255,0.3)',
+tarihcolor:'rgba(255,255,255,0.3)',
+daycolor:'rgba(255,255,255,0.3)',
+surnamecolor:'rgba(255,255,255,0.3)',
+passcolor:'rgba(255,255,255,0.3)',
+answercolor:'rgba(255,255,255,0.3)',
+yearcolor:'rgba(255,255,255,0.3)',
+emailcolor:'rgba(255,255,255,0.3)',
 
 
 
     },
     methods:{
         tiklama:function(Firstname,Surname,Tarih,Day,Year,email,password,answer)
-        {
+        { 
+            
              if(Firstname==null){this.namecolor='red'}
              if(Tarih==null){this.tarihcolor='red'}
              if(Day==null){this.daycolor='red'}
@@ -40,7 +41,7 @@ emailcolor:'#10a1de',
              if(email==null){this.emailcolor='red'}
              if(password==null){this.passcolor='red'}
             
-             if(answer==null){this.namecolor='red'}
+             if(answer==null){this.answercolor='red'}
         
             
 
---
+++ wepsite/cantlog.js
@@ -0,0 +1,6 @@
+Vue.component('kutular',{
+    template : '<div class=emailout></div><div class=kutu>dsadsadsadsadasdsa </div></div>'
+ });
+ var vm = new Vue({
+    el: '#kutu'
+ });
